553654996c7a3417bc7c19e964d3cb6ae8027061
saikalyan <cleansai@gmail.com>
2022-06-22 21:07:43 +0530

    Added @ConfigurationProperties to the service/rest/MovieRestService.java

--- src/main/java/com/adp/service/rest/MovieRestService.java
+++ src/main/java/com/adp/service/rest/MovieRestService.java
@@ -3,29 +3,39 @@ package com.adp.service.rest;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
 import com.adp.entiy.Movie;
 
-@Service
+@Component
+@ConfigurationProperties(ignoreUnknownFields = false, value = "movie.service")
 public class MovieRestService {
 
+	private static final String MOVIE_PATH = "movies/actor/";
 	@Autowired
 	private RestTemplate restTemplate;
 
-	@Value("${movie.service.url}")
-	private String url;
+	private String apihost;
 
 	public List<Movie> getAllMoviesByActorId(Integer id) {
-		ResponseEntity<List<Movie>> response = restTemplate.exchange(url + id, HttpMethod.GET, null,
+		String url = apihost + MOVIE_PATH + id;
+		ResponseEntity<List<Movie>> response = restTemplate.exchange(url, HttpMethod.GET, null,
 				new ParameterizedTypeReference<List<Movie>>() {
 				});
 		List<Movie> movies = response.getBody();
 		return movies;
+	}
+
+	public String getApihost() {
+		return apihost;
+	}
+
+	public void setApihost(String apihost) {
+		this.apihost = apihost;
 	}
 }
